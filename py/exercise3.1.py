10a600d928785a6eeed00617fde1548ebf6247d4
von110187 <vonpokoyo@gmail.com>
2026-04-02 12:16:01 +0800

    2nd commit

--- py/exercise3.1.py
+++ py/exercise3.1.py
@@ -1,1 +1,19 @@
 import random
+
+score = 0
+for i in range(3):
+    num1 = random.randrange(1, 100)
+    num2 = random.randrange(1, 100)
+    result = num1 + num2
+
+    while True:
+        try:
+            answer = int(input(f"{num1} + {num2}: "))
+            break
+        except ValueError:
+            print("Please enter a valid integer")
+    
+    if result == answer:
+        score += 1
+
+print(f"Your score is: {score}")
